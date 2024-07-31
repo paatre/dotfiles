29b257b73de29d5c4e9bbb604fb5aa3f62dac1de
Teemu Viikeri <tepavi@gmail.com>
2024-07-31 20:56:31 +0300

    Add Markdown preview plugin

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -897,6 +897,14 @@ require('lazy').setup({
       { '<c-\\>', '<cmd><C-U>TmuxNavigatePrevious<cr>' },
     },
   },
+  { -- Synced Markdown preview in browser
+    'iamcco/markdown-preview.nvim',
+    cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+    ft = { 'markdown' },
+    build = function()
+      vim.fn['mkdp#util#install']()
+    end,
+  },
 
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
