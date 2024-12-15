937f84c18488ee5699a3b008ab554447529b392b
Teemu Viikeri <tepavi@gmail.com>
2024-12-15 19:04:16 +0200

    Add some WIP Pyright config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -587,8 +587,21 @@ require('lazy').setup({
             },
           },
         },
-        pyright = {},
-        tsserver = {},
+        pyright = {
+          -- cmd = { 'docker', 'compose', 'exec', 'runserver', 'pyright-langserver', '--stdio' },
+          -- root_dir = function(fname)
+          --   return '~/Projects/haltu/wilma/wilma-desktop-demo'
+          -- end,
+          settings = {
+            python = {
+              analysis = {
+                autoSearchPaths = true,
+                diagnosticMode = 'openFilesOnly',
+                useLibraryCodeForTypes = true,
+              },
+            },
+          },
+        },
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
