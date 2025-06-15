976e49ba24bcac9e1e66fe98c00e52aff75e64ef
Teemu Viikeri <tepavi@gmail.com>
2025-06-15 23:16:29 +0300

    Write a better explanation comment for Comment plugin

--- nvim/.config/nvim/lua/custom/plugins/comment.lua
+++ nvim/.config/nvim/lua/custom/plugins/comment.lua
@@ -1,4 +1,4 @@
--- "gc" to toggle comments for visual regions/lines
+-- Provides commenting keymappings such as "gc" to toggle comments for visual regions/lines
 
 return {
 	"numToStr/Comment.nvim",
