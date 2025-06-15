1cf299d82f8354b30c25d769da63510809c715d2
Teemu Viikeri <tepavi@gmail.com>
2025-06-16 00:10:18 +0300

    Add automatic_installation and ensure_installed to Mason due to warnings

--- nvim/.config/nvim/lua/custom/plugins/lsp.lua
+++ nvim/.config/nvim/lua/custom/plugins/lsp.lua
@@ -227,6 +227,8 @@ return {
 		require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 
 		require("mason-lspconfig").setup({
+			automatic_installation = true,
+			ensure_installed = ensure_installed,
 			handlers = {
 				function(server_name)
 					local server = servers[server_name] or {}
