1d5e1de7d20609a68244d2a4b3b5cca00514a513
Teemu Viikeri <tepavi@gmail.com>
2026-09-15 23:51:18 +0300

    Indent Lua with tabs displayed two columns wide
    Lua files are now indented by using StyLua style: tabs in the file,
    display width chosen locally, which is the argument for tabs in the
    first place.
    
    Closes #6

---
+++ nvim/.config/nvim/after/ftplugin/lua.lua
@@ -0,0 +1,4 @@
+-- Lua files are formatted by StyLua, which indents with tabs. vim-sleuth
+-- detects that and sets 'noexpandtab' with 'shiftwidth' at 0 (follow
+-- 'tabstop'), so the display width is all that is left to choose.
+vim.bo.tabstop = 2
