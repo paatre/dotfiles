746b75cda571f2d53ab3fb37681b5a258f071816
Teemu Viikeri <tepavi@gmail.com>
2026-09-16 18:49:24 +0300

    Drop FixCursorHold.nvim from neotest's dependencies
    Its README states it is unnecessary after neovim/neovim#20198, which
    landed in Neovim 0.8. Nothing installed references it; neotest lists it
    in its own README but has no code dependency on it.

--- nvim/.config/nvim/lua/custom/plugins/neotest.lua
+++ nvim/.config/nvim/lua/custom/plugins/neotest.lua
@@ -10,7 +10,6 @@ return {
 	dependencies = {
 		"nvim-neotest/nvim-nio",
 		"nvim-lua/plenary.nvim",
-		"antoinemadec/FixCursorHold.nvim",
 		"nvim-treesitter/nvim-treesitter",
 		"marilari88/neotest-vitest",
 		"nvim-neotest/neotest-python",
